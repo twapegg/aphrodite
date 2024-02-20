223bdfd3a6ecf09b2a8bb18605e6f9d74a54a858
RamcerMediRecords <ramcer.dyteves@medirecords.com>
2024-02-20 14:27:43 +0800

    Add draft navbar

---
+++ components/NavBar.jsx
@@ -0,0 +1,12 @@
+export default function NavBar(){
+    return (
+<nav className="flex w-full justify-between">
+  <ul className="flex space-x-4">
+    <li>Home</li>
+    <li>About</li>
+    <li>Shop</li>
+    <li>Contact</li>
+  </ul>
+</nav>
+    );
+}
