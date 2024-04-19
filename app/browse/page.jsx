0af63fe7a352536d8477bdbf973f908c4dbf3e2b
John Stanley Altonaga <johntaltonaga@gmail.com>
2024-04-19 13:42:09 +0800

    Update page.jsx to include hero image and improve layout

--- app/browse/page.jsx
+++ app/browse/page.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Collection from "@/components/Collection";
+import Image from "next/image";
 
 const mockData = [
   {
@@ -34,17 +35,31 @@ const mockData = [
 
 const page = () => {
   return (
-    <div className="grid lg:grid-cols-3 gap-5 md:grid-cols-2 grid-cols-1">
-      {mockData.map((collection, index) => {
-        return (
-          <Collection
-            key={index}
-            img={collection.img}
-            category={collection.category}
-          />
-        );
-      })}
-    </div>
+    <>
+      <div className="relative h-screen">
+        <Image
+          src="/cover/jewelry.jpeg"
+          alt="Hero Image"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+        />
+        <div className="absolute inset-0 flex items-center justify-center">
+          <h1 className="text-4xl font-bold text-white">Jewelry Collections</h1>
+        </div>
+      </div>
+      <div className="py-10 grid lg:grid-cols-3 gap-5 md:grid-cols-2 grid-cols-1">
+        {mockData.map((collection, index) => {
+          return (
+            <Collection
+              key={index}
+              img={collection.img}
+              category={collection.category}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
