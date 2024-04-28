0941d4d2bdf089d83d64522b178c09357157e3d1
John Stanley Altonaga <johntaltonaga@gmail.com>
2024-04-28 22:20:30 +0800

    Update Image components to use unoptimized prop

--- components/Collection.jsx
+++ components/Collection.jsx
@@ -7,7 +7,7 @@ const Collection = ({ name, img }) => {
     <div className="group bg-gradient-to-t from-zinc-100 to-zinc-300" href="/">
       <div className="flex flex-col items-center justify-center p-5 text-black">
         <div className="relative transition-transform duration-300 group-hover:scale-110">
-          <Image src={img} alt={`${name}-image`} width={250} height={250} />
+          <Image src={img} alt={`${name}-image`} width={250} height={250} unoptimized={true}/>
         </div>
         <span className="text-3xl">{name}</span>
         <Link href="/collection/silver-jewelry" className="text-gray-700">
--- components/ImageDisplay.jsx
+++ components/ImageDisplay.jsx
@@ -11,6 +11,7 @@ const ImageDisplay = ({ source, alt }) => {
         className="object-cover"
         loading="lazy"
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        unoptimized={true}
       />
     </div>
   );
