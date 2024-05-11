de0eff434a1902c1b28280dce8fbedbee6eca686
John Stanley Altonaga <johntaltonaga@gmail.com>
2024-05-11 10:48:18 +0800

    chore: Refactor shopping bag route to use async/await syntax and improve error handling

--- app/api/shoppingbag/[id]/route.js
+++ app/api/shoppingbag/[id]/route.js
@@ -3,13 +3,15 @@ import Cart from "@/models/cart";
 import { connectToDB } from "@/utils/database";
 
 export const GET = async (request, { params }) => {
-    try {
-        await connectToDB()
+  try {
+    await connectToDB();
 
-        const cart = await Cart.find({ user: params.id })
+    const cart = await Cart.find({ user: params.id });
 
-        return new Response(JSON.stringify(cart), { status: 200 })
-    } catch (error) {
-        return new Response("Failed to fetch prompts created by user", { status: 500 })
-    }
-} 
+    return new Response(JSON.stringify(cart), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch prompts created by user", {
+      status: 500,
+    });
+  }
+};
