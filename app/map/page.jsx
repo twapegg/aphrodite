806ce74dccc70b385aeb2919783a29fbd6933a9a
Rusyl Anne Espina <ramcer.dyteves@medirecords.com>
2024-05-15 09:45:54 +0800

    made collections and changed font size of footer

--- app/map/page.jsx
+++ app/map/page.jsx
@@ -8,15 +8,15 @@ const page = () => {
       <NavBar bg={"white"} color="black" />
 
       <div className="bg-gray-300 pt-28 px-20 py-10 ml-3 mr-3">
-        <h1 className="text-4xl text-black font-bold pt-20 ">Navigation Bar</h1>
+        <h1 className="text-3xl text-black font-bold pt-20 ">Navigation Bar</h1>
         <div className=" grid lg:grid-cols-5 grid-cols-1 pt-20 text-black ">
-          <div className="text-2xl col-span-1 flex flex-col lg:pl-8 pt-8 sm:pb-5 ">
+          <div className="text-xl col-span-1 flex flex-col lg:pl-8 pt-8 sm:pb-5 ">
             <Link href="/">Menu</Link>
             <Link href="/">Contact Us</Link>
             <Link href="/">Cart</Link>
             <Link href="/">Account</Link>
           </div>
-          <div className="text-2xl col-span-1 flex flex-col sm:pb-5">
+          <div className="text-xl col-span-1 flex flex-col sm:pb-5">
             <h5 className="font-bold">Menu</h5>
             <Link href="/"> Home</Link>
             <Link href="/"> About</Link>
@@ -24,18 +24,18 @@ const page = () => {
             <Link href="/"> Contact</Link>
             <Link href="/"> Collection</Link>
           </div>
-          <div className="text-2xl col-span-1 flex flex-col sm:pb-5">
+          <div className="text-xl col-span-1 flex flex-col sm:pb-5">
             <h5 className="font-bold">Contact Us</h5>
             <Link href="/"> Call Us </Link>
             <Link href="/"> Email Us</Link>
             <Link href="/"> Frequently Ask Questions(FAQ)</Link>
           </div>
-          <div className="text-2xl col-span-1 flex flex-col sm:pb-5 ">
+          <div className="text-xl col-span-1 flex flex-col sm:pb-5 ">
             <h5 className="font-bold">Cart</h5>
             <Link href="/"> View Cart</Link>
             <Link href="/"> Checkout</Link>
           </div>
-          <div className="text-2xl col-span-1 flex flex-col sm:pb-5">
+          <div className="text-xl col-span-1 flex flex-col sm:pb-5">
             <h5 className="font-bold">Account</h5>
             <Link href="/"> Sign in</Link>
             <Link href="/">Transaction History</Link>
@@ -46,11 +46,11 @@ const page = () => {
         <h1 className="text-4xl text-black font-bold pt-20 ">Browse</h1>
 
         <div className="grid lg:grid-cols-3 grid-cols-1 pt-10 text-black">
-          <div className="text-2xl col-span-1 flex flex-col pt-8 lg:pl-8 sm:pb-5">
+          <div className="text-xl col-span-1 flex flex-col pt-8 lg:pl-8 sm:pb-5">
             <Link href="/">Collection</Link>
             <Link href="/">Category</Link>
           </div>
-          <div className="text-2xl col-span-1 flex flex-col sm:pb-5 ">
+          <div className="text-xl col-span-1 flex flex-col sm:pb-5 ">
             <h5 className="font-bold">Collection</h5>
             <Link href="/">Blossom</Link>
             <Link href="/">Les Gastons</Link>
@@ -60,7 +60,7 @@ const page = () => {
             <Link href="/">Dentelle</Link>
             <Link href="/">Vivienne</Link>
           </div>
-          <div className="text-2xl col-span-1 flex flex-col sm:pb-5">
+          <div className="text-xl col-span-1 flex flex-col sm:pb-5">
             <h5 className="font-bold">Category</h5>
             <Link href="/">Ring</Link>
             <Link href="/">Pendant</Link>
